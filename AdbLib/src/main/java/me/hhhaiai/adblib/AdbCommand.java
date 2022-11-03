23478d06238a69b3d81fe1d6d97b5618396a158f
sanbo <sanbo.xyz@gmail.com>
2022-11-03 17:09:15 +0800

    微调adb部分

--- AdbLib/src/main/java/me/hhhaiai/adblib/AdbCommand.java
+++ AdbLib/src/main/java/me/hhhaiai/adblib/AdbCommand.java
@@ -1,13 +1,7 @@
 package me.hhhaiai.adblib;
 
 import android.os.Looper;
-import android.os.SystemClock;
 import android.util.Base64;
-
-import me.hhhaiai.utils.Alog;
-import me.hhhaiai.utils.Streams;
-import me.hhhaiai.utils.Texts;
-import me.hhhaiai.utils.ref.ContentHolder;
 
 import java.io.File;
 import java.net.Socket;
@@ -18,8 +12,10 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
+import me.hhhaiai.utils.Alog;
+import me.hhhaiai.utils.Streams;
+import me.hhhaiai.utils.ref.ContentHolder;
 
 public class AdbCommand {
     private static String TAG = "sanbo.Mys";
@@ -120,7 +116,7 @@ public class AdbCommand {
             if (connection != null) {
                 connection.setFine(false);
             }
-            boolean result = generateConnection(ContentHolder.get().getFilesDir().getAbsolutePath());
+            boolean result = generateConnection();
             if (result) {
                 return retryExecAdb(cmd, wait);
             }
@@ -130,21 +126,17 @@ public class AdbCommand {
     }
 
 
-
     /**
      * 生成Adb连接，由所在文件生成，或创建并保存到相应文件
-     * @param cacheKeyDirPath
      * @return
      */
-    public static boolean generateConnection(String cacheKeyDirPath) {
-        if (Texts.isEmpty(cacheKeyDirPath)) {
-            Alog.e("the cacheKeyDirPath is null! please check!!");
-            return false;
-        }
+    public static boolean generateConnection() {
+
         if (connection != null && connection.isFine()) {
+            Alog.i(" connection is fine~~");
             return true;
         }
-        Alog.i("inside  generateConnection connection:" + connection);
+        Alog.i(" generateConnection connection:" + connection);
         if (connection != null) {
             try {
                 connection.close();
@@ -160,14 +152,15 @@ public class AdbCommand {
         AdbBase64 base64 = getBase64Impl();
 
         // 获取连接公私钥
-        File privKey = new File(ContentHolder.get().getFilesDir(), "privKey");
-        File pubKey = new File(ContentHolder.get().getFilesDir(), "pubKey");
+        File privKey = new File(ContentHolder.getCacheDir(), "privKey");
+        File pubKey = new File(ContentHolder.getCacheDir(), "pubKey");
 
         if (!privKey.exists() || !pubKey.exists()) {
             try {
+                Alog.d(" generateConnection privKey & pubKey not exists!");
                 crypto = AdbCrypto.generateAdbKeyPair(base64);
-                privKey.delete();
-                pubKey.delete();
+                privKey.deleteOnExit();
+                pubKey.deleteOnExit();
                 crypto.saveAdbKeyPair(privKey, pubKey);
             } catch (Throwable e) {
                 Alog.e(e);
@@ -180,8 +173,8 @@ public class AdbCommand {
                 Alog.e(e);
                 try {
                     crypto = AdbCrypto.generateAdbKeyPair(base64);
-                    privKey.delete();
-                    pubKey.delete();
+                    privKey.deleteOnExit();
+                    pubKey.deleteOnExit();
                     crypto.saveAdbKeyPair(privKey, pubKey);
                 } catch (Throwable ew) {
                     Alog.e(ew);
@@ -226,15 +219,8 @@ public class AdbCommand {
         connection = conn;
         Alog.i("ADB connected");
 
-//        if (DEVICE_ID == null) {
-//            DEVICE_ID = StringUtil.trim(execHighPrivilegeCmd("getprop ro.serialno"));
-//        }
-
-        // ADB成功连接后，开启ADB状态监测
-        startAdbStatusCheck();
-
-//        // 触发ADB连接状态监听
-//        LauncherApplication.getInstance().triggerAtTime(Trigger.TRIGGER_TIME_ADB_CONNECT);
+//        // ADB成功连接后，开启ADB状态监测, 暂缓该部分处理
+//        startAdbStatusCheck();
 
         return true;
     }
@@ -243,123 +229,123 @@ public class AdbCommand {
         return new AdbBase64() {
             @Override
             public String encodeToString(byte[] arg0) {
-                return Base64.encodeToString(arg0, 2);
+                return Base64.encodeToString(arg0, Base64.NO_WRAP);
             }
         };
     }
 
 
-    private static ScheduledExecutorService scheduledExecutorService;
-    private static volatile long LAST_RUNNING_TIME = 0;
-
-    /**
-     * 开始检查ADB状态
-     */
-    private static void startAdbStatusCheck() {
-        if (scheduledExecutorService == null) {
-            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        }
-
-
-        scheduledExecutorService.schedule(new Runnable() {
-            @Override
-            public void run() {
-                long currentTime = System.currentTimeMillis();
-                // 防止重复运行，14s内只能执行一次
-                if (currentTime - LAST_RUNNING_TIME < 14 * 1000) {
-                    return;
-                }
-
-                LAST_RUNNING_TIME = currentTime;
-                int result = checkAdbStatus();
-                if (result < 0) {
-                    Alog.e("[ADB异常]startAdbStatusCheck result:" + result);
-                    return;
-                }
-
-                // 15S 检查一次
-                scheduledExecutorService.schedule(this, 15, TimeUnit.SECONDS);
-            }
-        }, 15, TimeUnit.SECONDS);
-    }
-
-    /**
-     * 检查并尝试恢复ADB连接
-     * @return
-     */
-    private static int checkAdbStatus() {
-        String result = null;
-        try {
-            result = execAdbCmd("echo '1'", 5000);
-        } catch (Throwable e) {
-            Alog.e(e);
-        }
-
-        if (!Texts.equals("1", Texts.trim(result))) {
-            // 等2s再检验一次
-            SystemClock.sleep(2000);
-
-            boolean genResult = false;
-
-            // double check机制，防止单次偶然失败带来重连
-            String doubleCheck = null;
-            try {
-                doubleCheck = execAdbCmd("echo '1'", 5000);
-            } catch (Exception e) {
-                Alog.e(e);
-            }
-            if (!Texts.equals("1", Texts.trim(doubleCheck))) {
-                // 尝试恢复3次
-                for (int i = 0; i < 3; i++) {
-                    // 关停无用连接
-                    if (connection != null && connection.isFine()) {
-                        try {
-                            connection.close();
-                        } catch (Exception e) {
-                            Alog.e(e);
-                        } finally {
-                            connection = null;
-                        }
-                    }
-
-                    // 清理下当前已连接进程
-                    clearProcesses();
-
-                    // 尝试重连
-                    genResult = generateConnection(ContentHolder.get().getFilesDir().getAbsolutePath());
-                    if (genResult) {
-                        break;
-                    }
-                }
-
-                // 恢复失败
-                if (!genResult) {
-                    // 通知各个功能ADB挂了
-                    return -1;
-                }
-            }
-        }
-
-        return 0;
-    }
-
-
-    public static void clearProcesses() {
-        try {
-            for (Process p : processes) {
-                Alog.i("stop process: " + p.toString());
-                p.destroy();
-            }
-            processes.clear();
-            for (AdbStream stream : streams) {
-                Alog.i("stop stream: " + stream.toString());
-                Streams.close(stream);
-            }
-            streams.clear();
-        } catch (Throwable e) {
-            Alog.e(e);
-        }
-    }
+//    private static ScheduledExecutorService scheduledExecutorService;
+//    private static volatile long LAST_RUNNING_TIME = 0;
+//
+//    /**
+//     * 开始检查ADB状态
+//     */
+//    private static void startAdbStatusCheck() {
+//        if (scheduledExecutorService == null) {
+//            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+//        }
+//
+//
+//        scheduledExecutorService.schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                long currentTime = System.currentTimeMillis();
+//                // 防止重复运行，14s内只能执行一次
+//                if (currentTime - LAST_RUNNING_TIME < 14 * 1000) {
+//                    return;
+//                }
+//
+//                LAST_RUNNING_TIME = currentTime;
+//                int result = checkAdbStatus();
+//                if (result < 0) {
+//                    Alog.e("[ADB异常]startAdbStatusCheck result:" + result);
+//                    return;
+//                }
+//
+//                // 15S 检查一次
+//                scheduledExecutorService.schedule(this, 15, TimeUnit.SECONDS);
+//            }
+//        }, 15, TimeUnit.SECONDS);
+//    }
+//
+//    /**
+//     * 检查并尝试恢复ADB连接
+//     * @return
+//     */
+//    private static int checkAdbStatus() {
+//        String result = null;
+//        try {
+//            result = execAdbCmd("echo '1'", 5000);
+//        } catch (Throwable e) {
+//            Alog.e(e);
+//        }
+//
+//        if (!Texts.equals("1", Texts.trim(result))) {
+//            // 等2s再检验一次
+//            SystemClock.sleep(2000);
+//
+//            boolean genResult = false;
+//
+//            // double check机制，防止单次偶然失败带来重连
+//            String doubleCheck = null;
+//            try {
+//                doubleCheck = execAdbCmd("echo '1'", 5000);
+//            } catch (Exception e) {
+//                Alog.e(e);
+//            }
+//            if (!Texts.equals("1", Texts.trim(doubleCheck))) {
+//                // 尝试恢复3次
+//                for (int i = 0; i < 3; i++) {
+//                    // 关停无用连接
+//                    if (connection != null && connection.isFine()) {
+//                        try {
+//                            connection.close();
+//                        } catch (Exception e) {
+//                            Alog.e(e);
+//                        } finally {
+//                            connection = null;
+//                        }
+//                    }
+//
+//                    // 清理下当前已连接进程
+//                    clearProcesses();
+//
+//                    // 尝试重连
+//                    genResult = generateConnection();
+//                    if (genResult) {
+//                        break;
+//                    }
+//                }
+//
+//                // 恢复失败
+//                if (!genResult) {
+//                    // 通知各个功能ADB挂了
+//                    return -1;
+//                }
+//            }
+//        }
+//
+//        return 0;
+//    }
+//
+//
+//    public static void clearProcesses() {
+//        try {
+//            for (Process p : processes) {
+//                Alog.i("stop process: " + p.toString());
+//                p.destroy();
+//            }
+//            processes.clear();
+//            for (AdbStream stream : streams) {
+//                Alog.i("stop stream: " + stream.toString());
+//                Streams.close(stream);
+//            }
+//            streams.clear();
+//        } catch (Throwable e) {
+//            Alog.e(e);
+//        }
+//    }
 
     /************************************* 尝试系列******************************************/
 
